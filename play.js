f2cb8672f1d90cffdd36cc3c673555a74bb1da2a
Tali Saar <tali@rooster.co>
2017-07-19 18:18:19 -0700

    wrote a function for cloze questions

--- play.js
+++ play.js
@@ -1,3 +1,8 @@
+// NEED TO UNIFY THE FUNCTIONS FOR PLAYING A REGULAR CARD
+// AND A CLOZE CARD. ALSO NEED TO ADD THING TO SEE IF OUT
+// OF QUESTIONS AND NOT RUN NEXT QUESTION OR CLOZE
+
+
 var BasicCard = require("./BasicCard");
 var ClozeCard = require("./ClozeCard");
 var inquirer = require("inquirer");
@@ -10,6 +15,10 @@ var three = new ClozeCard ("We will we will rock you", "rock");
 var four = new ClozeCard ("A dog is a mans best friend", "dog");
 
 var questions_array = [one, two, three, four];
+
+var Basic_cards_array = [one, two]; // temporary for fun
+var ClozeCard_array = [three, four]; // temporary for fun
+
 
 var counter = 0;
 var correct_answer;
@@ -51,8 +60,45 @@ correct_answer = questions_array[counter].back.toLowerCase();
 
 }
 
+
+// var nextCloze = function () {
+
+
+// 	correct_answer = ClozeCard_array[counter].cloze.toLowerCase();
+
+// 		inquirer.prompt([ 
+
+//       {
+//         name: "clozequestion",
+//         message: "Complete the sentence: "+ClozeCard_array[counter].partial
+//       }
+//     ]).then(function(answers) {
+
+//     	user_answer = answers.clozequestion.toLowerCase();
+
+//     	if (user_answer === correct_answer){
+
+//     		    	console.log("correct!");
+//     		    	counter++;
+//     		    	console.log(counter);
+//     				nextCloze();
+
+//     	}
+
+//     	else {
+//     		console.log("wrong!");
+//     		counter++;
+//     		nextCloze();
+//     	}
+
+
+
+// });
+
+// };
 nextQuestion();
 
+// nextCloze();
 
 
 
